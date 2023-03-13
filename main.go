3d4db8fddfde874d32a5caf4242764b365b32f61
sny430 <shinya430.g@gmail.com>
2023-03-13 14:09:47 +0900

    :loud_sound: add logger

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -10,6 +9,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 
 	"hackz-allo/api"
 )
@@ -18,28 +18,27 @@ func gormConnect() *gorm.DB {
 	dsn := "postgres://" + os.Getenv("AZURE_PG_USER") + ":" + os.Getenv("AZURE_PG_PASSWORD") + "@hackzallopostgres.postgres.database.azure.com/postgres?sslmode=require"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("DB error(Init): ", err)
-	} else {
-		fmt.Println("DB Connected!")
+		panic(err)
 	}
 	return db
 }
 
 func main() {
 
+	// 環境変数の読み込み
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
 	}
 
-	var a []int
+	// DB初期設定
 	db := gormConnect()
-	db.Find(&a)
+	db.Logger = db.Logger.LogMode(logger.Info)
 
-	// インスタンスを作成
+	// インスタンス作成
 	e := echo.New()
 
-	// ミドルウェアを設定
+	// ミドルウェア設定
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
@@ -61,6 +60,6 @@ func main() {
 	e.GET("/login", api.LogIn)
 	e.GET("/signup", api.SignUp)
 
-	// サーバーをポート番号8080で起動
+	// サーバー起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
