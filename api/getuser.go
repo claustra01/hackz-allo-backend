bb9d1ffb1f0caa5d6e83d023cd9b9d280f832b6a
sny430 <shinya430.g@gmail.com>
2023-03-14 02:17:57 +0900

    :sparkles: get_user api

--- api/getuser.go
+++ api/getuser.go
@@ -4,8 +4,30 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"hackz-allo/database"
 )
 
+type oGetUser struct {
+	UserId string
+	Name   string
+}
+
 func GetUser(c echo.Context) error {
-	return c.String(http.StatusOK, "")
+
+	db := database.Connect()
+	token := c.QueryParam("token")
+
+	// ユーザー情報取得
+	array := []database.User{}
+	db.Find(&array)
+	for _, u := range array {
+		if u.Id.String() == token {
+			obj := new(oGetUser)
+			obj.UserId = u.UserId
+			obj.Name = u.Name
+			return c.JSON(http.StatusOK, obj)
+		}
+	}
+	return c.JSON(http.StatusOK, nil)
 }
